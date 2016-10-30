58f4e1f832f2991ccb6ebd4bc617e89b0be5a08d
MF1996 <makam-elchahid@outlook.fr>
2016-10-30 19:57:16 +0000

    Update EmployeTempsPartiel.java

--- Gestion-de-la-paie/src/Paie_employer/EmployeTempsPartiel.java
+++ Gestion-de-la-paie/src/Paie_employer/EmployeTempsPartiel.java
@@ -3,36 +3,3 @@ package Paie_employer;
 /**
  * Created by HAMALI on 25/10/16.
  */
-public class EmployePleinTemps extends Employe {
-    private double montantheb;
-    private double prime;
-
-    public double getPrime() {
-        return prime;
-    }
-
-    public void setPrime(double prime) {
-        this.prime = prime;
-    }
-
-    public double getMontantheb() {
-
-        return montantheb;
-    }
-
-    public void setMontantheb(double montantheb) {
-        this.montantheb = montantheb;
-    }
-
-    public EmployePleinTemps(String nom, String job
-    ) {
-        super(nom, job);
-
-    }
-
-    @Override
-    public double calculerPaie() {
-        setSalaire((montantheb*4+prime));
-        return getSalaire();
-    }
-}
