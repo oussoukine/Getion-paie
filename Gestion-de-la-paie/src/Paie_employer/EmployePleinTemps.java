7698032dffe44a796cba9e3f09bae0dead34c9e5
MF1996 <makam-elchahid@outlook.fr>
2016-10-30 19:58:21 +0000

    Update EmployePleinTemps.java

--- Gestion-de-la-paie/src/Paie_employer/EmployePleinTemps.java
+++ Gestion-de-la-paie/src/Paie_employer/EmployePleinTemps.java
@@ -3,10 +3,36 @@ package Paie_employer;
 /**
  * Created by HAMALI on 25/10/16.
  */
-public class EmployePleinTemps extends Employe{
+public class EmployePleinTemps extends Employe {
+    private double montantheb;
+    private double prime;
 
-    public EmployePleinTemps(String nom, String fonct, double salaire)
-    {
-        super(nom, fonct, salaire);
+    public double getPrime() {
+        return prime;
+    }
+
+    public void setPrime(double prime) {
+        this.prime = prime;
+    }
+
+    public double getMontantheb() {
+
+        return montantheb;
+    }
+
+    public void setMontantheb(double montantheb) {
+        this.montantheb = montantheb;
+    }
+
+    public EmployePleinTemps(String nom, String job
+    ) {
+        super(nom, job);
+
+    }
+
+    @Override
+    public double calculerPaie() {
+        setSalaire((montantheb*4+prime));
+        return getSalaire();
     }
 }
