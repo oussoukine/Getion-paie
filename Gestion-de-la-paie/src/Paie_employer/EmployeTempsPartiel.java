f1de4a6241bbb10a93676dd386e7a113e442a391
MF1996 <makam-elchahid@outlook.fr>
2016-10-30 19:59:49 +0000

    Update EmployeTempsPartiel.java

--- Gestion-de-la-paie/src/Paie_employer/EmployeTempsPartiel.java
+++ Gestion-de-la-paie/src/Paie_employer/EmployeTempsPartiel.java
@@ -3,3 +3,27 @@ package Paie_employer;
 /**
  * Created by HAMALI on 25/10/16.
  */
+public class EmployeTempsPartiel extends Employe {
+    private double montantHeure;
+    final int heure = 35;
+
+    public double getMontantHeure() {
+        return montantHeure;
+    }
+
+    public void setMontantHeure(double montantHeure) {
+        this.montantHeure = montantHeure;
+    }
+
+
+    public EmployeTempsPartiel(String nom, String job) {
+        super(nom, job);
+
+    }
+
+    @Override
+    public double calculerPaie() {
+        setSalaire((montantHeure * heure));
+        return getSalaire();
+    }
+}
