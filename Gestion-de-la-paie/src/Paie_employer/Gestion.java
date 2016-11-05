271e6e8745d0c85e1dc91da0c480baa549ee5cb4
Fatima1930 <labonne1930@hotmail.fr>
2016-11-05 20:07:14 +0100

    j ai ajoute 2 commentaire

--- Gestion-de-la-paie/src/Paie_employer/Gestion.java
+++ Gestion-de-la-paie/src/Paie_employer/Gestion.java
@@ -9,20 +9,22 @@ package Paie_employer;
 public class Gestion{
 
 public static void main(String args[]){
+        //**creation du tableau**/
 Employe ep[] = new Employe[3];
+        //**remplissage du tableau**//
         ep[0] = new Employe("kadour", "pdg");
-        ep[1] = new EmployeTempsPartiel("adam", "cuisinier");
-        ep[2] = new EmployePleinTemps("haouari", "plombier");
+        ep[1] = new EmployeTempsPartiel("Salim ", "comptable");
+        ep[2] = new EmployePleinTemps("Houari", "plombier");
 
         ep[0].setSalaire(2500);
         System.out.print(ep[0].afficheCheque());
         System.out.print("=================");
-        ((EmployeTempsPartiel) ep[1]).setMontantHeure(2500);
+        ((EmployeTempsPartiel) ep[1]).setMontantHeure(2500 DA);
         ((EmployeTempsPartiel) ep[1]).calculerPaie();
         System.out.print(ep[1].afficheCheque());
         System.out.print("================");
-        ((EmployePleinTemps) ep[2]).setMontantheb(2500);
-        ((EmployePleinTemps) ep[2]).setPrime(500);
+        ((EmployePleinTemps) ep[2]).setMontantheb(2500 DA);
+        ((EmployePleinTemps) ep[2]).setPrime(500 DA);
         ((EmployePleinTemps) ep[2]).calculerPaie();
         System.out.print(ep[2].afficheCheque());
 }
